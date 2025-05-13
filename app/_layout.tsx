9a6ee12694e41be58bb762f356c3f9444bf5e676
samay-bacancy <samay.mangukiya@bacancy.com>
2025-05-13 19:20:01 +0530

    Setup main layout file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,4 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
@@ -7,23 +6,27 @@ import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+	const colorScheme = useColorScheme();
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+	const isLoggedIn = true;
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
-}
+	return (
+		<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+			<Stack>
+				<Stack.Screen
+					name="index"
+					options={{
+						title: 'Login',
+					}}
+				/>
+
+				<Stack.Protected guard={isLoggedIn}>
+					<Stack.Screen name="(app)"  options={{ headerShown: false }} />
+				</Stack.Protected>
+
+				<Stack.Screen name="+not-found" />
+			</Stack>
+			<StatusBar style="auto" />
+		</ThemeProvider>
+	);
+};
